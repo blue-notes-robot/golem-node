d3ae32567f1607ee80596b89a6f8c43213e96f61
blue-notes-robot <none>
2021-04-05 17:41:38 +0100

    Newline fix

--- scripts/log_active_nodes.sh
+++ scripts/log_active_nodes.sh
@@ -4,7 +4,7 @@ while true;
 do
     echo -n "`date +"[%F %H:%M:%S]"` "
     echo -n "Nodes active: $($script_full_path/get_active_nodes.sh) "
-    echo -n "Total processed last 1hr: $($script_full_path/get_processed_last_hr.sh)"
+    echo "Total processed last 1hr: $($script_full_path/get_processed_last_hr.sh)"
     sleep 60
 
 done
