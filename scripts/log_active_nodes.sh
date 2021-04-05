b4097207a9037b599786ec2a02345f2cd29e76b7
blue-notes-robot <none>
2021-04-05 17:39:42 +0100

    Loop script logging Active nodes and total processed last hour

--- scripts/log_active_nodes.sh
+++ scripts/log_active_nodes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+script_full_path=$(dirname "$0")
 while true;
 do
-    echo -n `date +"[%F %H:%M:%S]"`;
-    echo " Nodes active: $(pgrep vmrt | wc -l)";
-    sleep 60;
+    echo -n "`date +"[%F %H:%M:%S]"` "
+    echo -n "Nodes active: $($script_full_path/get_active_nodes.sh) "
+    echo -n "Total processed last 1hr: $($script_full_path/get_processed_last_hr.sh)"
+    sleep 60
+
 done
