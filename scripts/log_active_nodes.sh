4a7d5dc6ef6f6597bdbca99be1d632bcb9295af7
blue-notes-robot <none>
2021-04-05 17:45:06 +0100

    Typo fix

--- scripts/log_active_nodes.sh
+++ scripts/log_active_nodes.sh
@@ -4,6 +4,6 @@ while true;
 do
     echo -n "$(date +"[%F %H:%M:%S]") "
     echo -n "Nodes active: $("$script_full_path"/get_active_nodes.sh) "
-    echo "Total processed last 1hr: $("$script_full_path"/get_processed_last_hr.sh)"
+    echo "Total processed last 1h: $("$script_full_path"/get_processed_last_hr.sh)"
     sleep 60
 done
