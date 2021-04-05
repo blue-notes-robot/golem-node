f9b98d7f55e03e6366895502edfcfaaca3033732
blue-notes-robot <none>
2021-04-05 17:44:10 +0100

    Some refactoring

--- scripts/log_active_nodes.sh
+++ scripts/log_active_nodes.sh
@@ -2,9 +2,8 @@
 script_full_path=$(dirname "$0")
 while true;
 do
-    echo -n "`date +"[%F %H:%M:%S]"` "
-    echo -n "Nodes active: $($script_full_path/get_active_nodes.sh) "
-    echo "Total processed last 1hr: $($script_full_path/get_processed_last_hr.sh)"
+    echo -n "$(date +"[%F %H:%M:%S]") "
+    echo -n "Nodes active: $("$script_full_path"/get_active_nodes.sh) "
+    echo "Total processed last 1hr: $("$script_full_path"/get_processed_last_hr.sh)"
     sleep 60
-
 done
