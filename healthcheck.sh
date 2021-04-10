225f182ee345fc4c48ce9daf74621a47fb3aa397
blue-notes-robot <none>
2021-04-10 10:08:00 +0100

    Added some echo in healthcheck for debuging, will be anyways ignored by autoheal

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-if grep -q ERROR ~/.local/share/yagna/yagna_rCURRENT.log; then exit 1; else exit 0; fi
-golemsp status || exit 1
+if grep -q ERROR root/.local/share/yagna/yagna_rCURRENT.log
+    then
+        echo ERROR found!; exit 1
+    else
+        echo No error found in logs
+fi
+if golemsp status
+    then
+        echo golemsp is OK
+    else
+        echo golemsp failed
+        exit 1
+fi
