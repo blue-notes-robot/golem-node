6052aa73f2364cfaefa09894d406bd1fa1df3718
blue-notes-robot <none>
2021-04-16 13:38:18 +0100

    Adding invoice error check to healthcheck.sh

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # commented for now as since pre-rel-v0.6.4-rc5 there seem to be no hangups anymore
-#if grep -q ERROR /root/.local/share/yagna/yagna_rCURRENT.log
-#    then
-#        echo ERROR found!; exit 1
-#    else
-#        echo No error found in logs
-#fi
+if grep -q "Error sending invoice" /root/.local/share/ya-provider/ya-provider_rCURRENT.log
+    then
+        echo ERROR found!; exit 1
+    else
+        echo No error found in logs
+fi
 if golemsp status
     then
         echo golemsp is OK
